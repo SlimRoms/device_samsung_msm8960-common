49d45226bd54918c913625640ab6345bf909e7c4
Steve Kondik <shade@chemlab.org>
2013-02-14 20:29:10 -0800

    add cortex-a9 target-cflags
    Change-Id: I664cf38586db9550cb4e6c8973bd6cee50c191ef

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -27,10 +27,9 @@ TARGET_ARCH_VARIANT_CPU := cortex-a9
 # Architecture
 TARGET_CPU_SMP := true
 
-# Flags for Krait CPU
+# Flags for Krait CPU / tune for cortex-a9
 COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
-TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+TARGET_EXTRA_CFLAGS := -mtune=cortex-a9 -mcpu=cortex-a9
 
 # Krait optimizations
 TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
