59b42a9a1f0e303c084bb648bdd9ecc6a2f86707
Steve Kondik <shade@chemlab.org>
2013-03-11 22:40:11 -0700

    msm8960: Changes for 3.4 kernel update
     * Update headers and use new ION API.
    
    Change-Id: Ib90c19dbdbc667eb008f1b30827145d70d0ed7aa
    
    Conflicts:
    	BoardConfigCommon.mk
    	include/linux/msm_vidc_dec.h

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -27,9 +27,10 @@ TARGET_ARCH_VARIANT_CPU := cortex-a9
 # Architecture
 TARGET_CPU_SMP := true
 
-# Flags for Krait CPU / tune for cortex-a9
-COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
-TARGET_EXTRA_CFLAGS := -mtune=cortex-a9 -mcpu=cortex-a9
+# Flags for Krait CPU
+COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64 -DNEW_ION_API=1
+TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 
 # Krait optimizations
 TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
@@ -82,12 +83,6 @@ BOARD_USES_ALSA_AUDIO := true
 BOARD_HAVE_SAMSUNG_AUDIO := true
 BOARD_USES_FLUENCE_INCALL := true
 BOARD_USES_SEPERATED_AUDIO_INPUT := true
-
-# Workaround for missing symbols in camera
-BOARD_NEEDS_MEMORYHEAPPMEM := true
-
-# We still have the old ION API
-BOARD_HAVE_OLD_ION_API := true
 
 # Kernel time optimization
 # temp remove - causing issues with short/long presses
