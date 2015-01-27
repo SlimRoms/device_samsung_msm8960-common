LineageOS/android_device_samsung_msm8960-common

build: Avoid poisoning the host binaries with bad ABIs
the vector class symbols are only needed on the target. Don't inject
(or expect) them into the host binaries to avoid multi-device build
conflicts and ABI breakage

Change-Id: Idffa6ed6621e1d51807468a94bb22212dc128601

BoardConfigCommon.mk
@@ -41,9 +41,9 @@ QCOM_FLUENCE_ENABLED := false
 BOARD_HAVE_BLUETOOTH := true
 
 # Camera
-COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 COMMON_GLOBAL_CFLAGS += -DQCOM_BSP_CAMERA_ABI_HACK
 COMMON_GLOBAL_CFLAGS += -DSAMSUNG_CAMERA_HARDWARE
+TARGET_GLOBAL_CPPFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 TARGET_PROVIDES_CAMERA_HAL := true
 USE_DEVICE_SPECIFIC_CAMERA := true
 

SlimRoms/device_samsung_msm8960-common

BoardConfigCommon.mk
@@ -41,9 +41,9 @@ QCOM_FLUENCE_ENABLED := false
 BOARD_HAVE_BLUETOOTH := true
 
 # Camera
-COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 COMMON_GLOBAL_CFLAGS += -DQCOM_BSP_CAMERA_ABI_HACK
 COMMON_GLOBAL_CFLAGS += -DSAMSUNG_CAMERA_HARDWARE
+TARGET_RELEASE_CPPFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 TARGET_PROVIDES_CAMERA_HAL := true
 USE_DEVICE_SPECIFIC_CAMERA := true
 