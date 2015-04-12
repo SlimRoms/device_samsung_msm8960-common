6051c392e1be5cff62c89ffae310a9f38c7367a7
Dave Daynard <nardholio@gmail.com>
2015-04-12 18:57:38 -0400

    msm8960-common: DO NOT MERGE: switch back to nuplayer
    Conflicts:
    	msm8960.mk
    Change-Id: Id1fe651494a8ec831b6a833e98aaceb23ccc5f10

--- msm8960.mk
+++ msm8960.mk
@@ -66,9 +66,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=131072
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.media.use-awesome=true
-
-PRODUCT_PROPERTY_OVERRIDES += \
     persist.data_netmgrd_nint=16 \
     persist.radio.add_power_save=1 \
     persist.radio.mode_pref_nv10=1 \
